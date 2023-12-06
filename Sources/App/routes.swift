5002d76d258d7d2e1abdf2bd0c9b4c8581ee22f8
bashta <erisonve@gmail.com>
2023-12-06 18:35:16 +0100

    Add HTML respose to hello route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,34 @@
 import Vapor
+import SwiftHtml
 
 func routes(_ app: Application) throws {
+    
     app.get { req async in
         "It works!"
     }
-
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    
+    app.routes.get("hello") { req -> Response in
+        let doc = Document(.html) {
+            Html {
+                Head {
+                    Title("Hello, Head!")
+                }
+                Body {
+                    H1("Hello, Body!")
+                }
+            }
+        }
+        let body = DocumentRenderer(
+            minify: false,
+            indent: 4
+        )
+            .render(doc)
+        return Response(
+            status: .ok,
+            headers: [
+                "Content-Type": "text/html; charset=utf-8"
+            ],
+            body: .init(string: body)
+        )
     }
 }
