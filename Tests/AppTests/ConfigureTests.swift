fa4fad6103959401ca37f75580e8544a7d268674
Erison Veshi <erisonve@gmail.com>
2023-12-07 12:26:47 +0100

    Enable FileMiddleware

---
+++ Tests/AppTests/ConfigureTests.swift
@@ -0,0 +1,19 @@
+/**
+ This test case verifies the configuration of the application.
+ It creates an instance of the Application in testing mode, configures it using the `configure` function, and then performs the following assertions:
+ - Checks if the FileMiddleware is registered as a middleware.
+ - Checks if there are registered routes in the application.
+
+ If any of the assertions fail, an error will be thrown.
+ */
+func testConfigure_ContainsFileMiddleware() async throws {
+    let app = Application(.testing)
+    defer { app.shutdown() }
+    try await configure(app)
+
+    // Test that the FileMiddleware is registered
+    XCTAssertTrue(app.middleware.contains { $0 is FileMiddleware })
+
+    // Test that the routes are registered
+    XCTAssertTrue(app.routes.all.count > 0)
+}
