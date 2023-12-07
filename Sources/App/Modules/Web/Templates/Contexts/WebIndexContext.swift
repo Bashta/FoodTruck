b4a631ea43e05b3bfb8235f8e4be488f9fe93e80
bashta <erisonve@gmail.com>
2023-12-07 20:47:19 +0100

    Add WebIndex template

---
+++ Sources/App/Modules/Web/Templates/Contexts/WebIndexContext.swift
@@ -0,0 +1,20 @@
+//
+//  WebIndexContext.swift
+//
+//
+//  Created by Erison on 07/12/2023.
+//
+
+public struct WebIndexContext {
+    
+    public let title: String
+    public let message: String
+    
+    public init(
+        title: String,
+        message: String
+    ) {
+        self.title = title
+        self.message = message
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,21 +1,6 @@
 import Vapor
 import SwiftHtml
 
-struct DefaultTemplate: TemplateRepresentable {
-    let title: String
-    
-    func render(_ req: Request) -> Tag {
-        Html {
-            Head {
-                Title(title)
-            }
-            Body {
-                H1(title)
-            }
-        }
-    }
-}
-
 func routes(_ app: Application) throws {
     
     app.get { req async in
@@ -23,7 +8,11 @@ func routes(_ app: Application) throws {
     }
     
     app.routes.get("hello") { req -> Response in
-        let template = DefaultTemplate(title: "Hello Teplate Rendering")
+        let template = WebIndexTemplate(
+            WebIndexContext(
+                title: "Food Truck",
+                message: "Welcome to Food Truck")
+        )
         return req.templates.renderHtml(template)
     }
 }
