d7879ee5cec1a13e21e2c181befde6fd6091ef66
bashta <erisonve@gmail.com>
2023-12-14 10:38:42 +0100

    Remove unused test

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -2,13 +2,5 @@
 import XCTVapor
 
 final class AppTests: XCTestCase {
-    func testHelloWorld() async throws {
-        let app = Application(.testing)
-        defer { app.shutdown() }
-        try await configure(app)
 
-        try app.test(.GET, "/blog/", afterResponse: { res in
-            XCTAssertEqual(res.status, .ok)
-        })
-    }
 }
